0b22743552645bb5577b959cb8281bebb023b428
Mukthar Ayappalli <muktharayappalli@Mukthars-MacBook-Air.local>
2024-11-20 13:53:48 +0300

    newwww

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*tyqt@yf(tpm19+zdydb)n-xry4k=^=v+mi(fr*yb1ws%0(fam
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ]
 
 
 # Application definition
