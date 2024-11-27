1f8e03f104752855d575d3a9148db6936ee6a6b5
Mukthar Ayappalli <muktharayappalli@Mukthars-MacBook-Air.local>
2024-11-27 16:28:00 +0300

    mmm

--- backend/db.py
+++ backend/db.py
@@ -6,5 +6,5 @@ from pymongo.server_api import ServerApi
 
 MONGO_DB = os.getenv("MONGO_DB", "mydatabase")
 uri = "mongodb+srv://backend:12345@cluster0.e7itw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-client = MongoClient(uri, server_api=ServerApi('1'),tlsCAFile=certifi.where())
+client = MongoClient(uri, server_api=ServerApi('1'),tls=True,tlsCAFile=certifi.where())
 dbconn = client[MONGO_DB]
