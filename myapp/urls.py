a209f618e0897114b4f16fe2e00fbb6d2eea046d
Mukthar Ayappalli <muktharayappalli@Mukthars-MacBook-Air.local>
2024-12-03 12:23:28 +0300

    rrr

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('login/',views.sign_in,name='login')
+    path('login/',views.sign_in,name='login'),
+    path('add_product/',views.add_product,name='add_product'),
+    path('get_products/',views.get_products,name='get_products')
 ]
